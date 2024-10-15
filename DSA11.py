ed539414f6c2e938f322db5849af565ad5b0b2c9
Subrahmanya Gaonkar <148525245+negativenagesh@users.noreply.github.com>
2024-10-15 16:26:01 +0530

    Update and rename DSA11.txt to DSA11.py aad commented

---
+++ DSA11.py
@@ -0,0 +1,84 @@
+# Question ---->
+
+# You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+# Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+# The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+# Example 1:
+
+# Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+# Output: [1,2,2,3,5,6]
+# Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+# The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+# Example 2:
+
+# Input: nums1 = [1], m = 1, nums2 = [], n = 0
+# Output: [1]
+# Explanation: The arrays we are merging are [1] and [].
+# The result of the merge is [1].
+# Example 3:
+
+# Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+# Output: [1]
+# Explanation: The arrays we are merging are [] and [1].
+# The result of the merge is [1].
+# Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+ 
+
+# Constraints:
+
+# nums1.length == m + n
+# nums2.length == n
+# 0 <= m, n <= 200
+# 1 <= m + n <= 200
+# -109 <= nums1[i], nums2[j] <= 109
+
+# Answer --->
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        
+        p1=m-1
+        p2=n-1
+        
+        p=m+n-1
+        
+        while p1>=0 and p2>=0:
+            if nums1[p1]>nums2[p2]:
+                nums1[p]=nums1[p1]
+                p1-=1
+            
+            else:
+                nums1[p]=nums2[p2]
+                p2-=1
+            p-=1
+        
+        while p2>=0:
+            nums1[p]=nums2[p2]
+            p2-=1
+            p-=1
+            
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+
+# Result ---->
+
+# Your input:
+# [1,2,3,0,0,0]
+# 3
+# [2,5,6]
+# 3
+
+# Output:
+# [1,2,2,3,5,6]
+
+# Expected:
+# [1,2,2,3,5,6]
+
