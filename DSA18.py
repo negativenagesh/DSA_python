f26f4c59ffe72a14b024ff008969f0d4f5f4ab6c
Subrahmanya Gaonkar <148525245+negativenagesh@users.noreply.github.com>
2024-10-15 16:32:18 +0530

    Update and rename DSA18.txt to DSA18.py

---
+++ DSA18.py
@@ -0,0 +1,61 @@
+# Question --->
+
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+# Example 2:
+
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+# Example 3:
+
+# Input: s = " "
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric characters.
+# Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 2 * 105
+# s consists only of printable ASCII characters.
+
+# Answer ---->
+
+class Solution(object):
+    def isPalindrome(self, s):
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
+
+# Result --->
+
+# Your input:
+# "A man, a plan, a canal: Panama"
+
+# Output:
+# true
+
+# Expected:
+# true
